0a07fbc639b4111b2295db950979740f834cad9c
Chandlerac <chandler4acjh@gmail.com>
2024-05-14 14:05:07 -0400

    I did it!

---
+++ check2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using check2;
+
+Console.WriteLine("How many music artist do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<Artist>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var artist = new Artist();
+
+    Console.WriteLine("Enter the artist name: ");
+    artist.Name = Console.ReadLine();
+
+    Console.WriteLine("Enter genre: ");
+    artist.Genre = Console.ReadLine();
+
+    recordList.Add(artist);
+}
+
+// Print out the list of records using Console.WriteLine()
+
+foreach (var artist in recordList)
+{
+    Console.WriteLine($"Artist name:  {artist.Name}  Genre:  {artist.Genre}");
+}
